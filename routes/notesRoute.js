27bcdca01133d91051b117256c24557d1a7c0190
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-02-10 17:43:32 +0530

    feat-delete,getsingle,getall

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { UpdateNoteController, createNoteController } from '../controllers/noteController.js';
+import { DeleteNoteController, UpdateNoteController, createNoteController, getAllNotesController, getSingleNoteController } from '../controllers/noteController.js';
 
 const router=express.Router();
 //creating note route
 router.post('/create-note',createNoteController);
 //updating note route
-router.put('/update-note/:id',UpdateNoteController)
+router.put('/update-note/:id',UpdateNoteController);
+//deleteing note route
+router.delete('/delete-note/:id',DeleteNoteController);
+//get single note route
+router.get('/getsingle-note/:id',getSingleNoteController);
+//get all notes route
+router.get('/getAll-Notes',getAllNotesController);
 export default router;
